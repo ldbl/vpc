acd8c25c23cd63915e884b2a5072795b79a98408
ldbl <stanfold@gmail.com>
2020-08-19 15:57:26 -0400

    openvpn provision

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -5,10 +5,12 @@ resource "aws_instance" "openvpn" {
   subnet_id     = aws_subnet.public-subnet-1.id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.ec2_public_security_group.id]
-  count = 0
+  count = 1
   tags = {
     Name = "OpenVPN"
   }
   depends_on = [aws_eip.elastic-ip-for-nat-gw]
-
+  provisioner "local-exec" {
+        command = "sleep 120; ansible-playbook -u ubuntu --private-key ~/.ssh/root-key.pem -i '${aws_instance.openvpn[count.index].public_ip},' provision/provision.yml"
+  }
 }
